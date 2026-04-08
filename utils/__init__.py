0358726272e463b7493312c4f0262ebfd2b20e42
Oswaldo Morales <morales.oswaldo151@gmail.com>
2026-04-08 16:56:17 -0600

    corrigiendo el llamado a funciones en el archivo

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,2 +1,2 @@
-from io_helpers import leer_archivo,reporte_detalle,reporte_resumen
-from validators import validar_presion,validar_unidad,validar_datos
+from .io_helpers import leer_archivo,reporte_detalle,reporte_resumen
+from .validators import validar_presion,validar_unidad,validar_datos
