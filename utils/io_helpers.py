a6eb4a60eb2103ab84c9d13ff11adce62582435f
Oswaldo Morales <morales.oswaldo151@gmail.com>
2026-04-07 22:38:09 -0600

    Añadiendo funciones para escribir los reportes

--- utils/io_helpers.py
+++ utils/io_helpers.py
@@ -6,8 +6,8 @@ def leer_archivo(archivo_entrada):
     #Lista vacia donde se almacenaran los registros validos
     datos = []
 
-    with open(archivo_entrada, 'r', encoding= 'utf-8') as archivo:
-        lineas = archivo.readlines()
+    with open(archivo_entrada, 'r', encoding= 'utf-8') as entrada:
+        lineas = entrada.readlines()
 
         #Si el archivo esta vacio devuele la lista "datos" vacia
         if not lineas:
@@ -38,8 +38,33 @@ def leer_archivo(archivo_entrada):
 
     return datos
 
+def reporte_detalle(datos_ordenados,ruta_archivo):
+
+    encabezados= ["id_revision","vehiculo","tipo_vehiculo","presion_bar","estado_presion"]
+
+    with open(ruta_archivo, 'w', encoding= 'utf-8') as archivo:
+        #Escribir los encabezados
+        archivo.write(','.join(encabezados) + '\n')
+
+        #Escribir datos
+        for dato in datos_ordenados:
+            linea = f"{dato.id_revision},{dato.vehiculo},{dato.tipo_vehiculo},"
+            linea +=f"{dato.presion_bar:.2f},{dato.clasificacion()}"
+            archivo.write(linea + '\n') 
 
 
+def reporte_resumen(datos_dic,ruta_archivo):
+    encabezados = ["tipo_vehiculo","conteo","promedio","maximo"]
+
+
+    with open(ruta_archivo,'w',encoding='utf-8') as archivo:
+        #Escrbir los encabezados
+        archivo.write(','.join(encabezados) + '\n')
+
+        #Desempaquetando y escribiendo los datos
+        for tipo,dato in datos_dic.items():
+            linea = f"{tipo},{dato['conteo']},{dato['promedio']},{dato['maximo']}"
+            archivo.write(linea + '\n')
 
 
 
