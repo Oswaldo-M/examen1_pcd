30694814f417615862c97699ae92625e4f795139
Oswaldo Morales <morales.oswaldo151@gmail.com>
2026-04-07 21:00:56 -0600

    Agregando  funciones para validar que los datos sean validos

---
+++ utils/validators.py
@@ -0,0 +1,40 @@
+
+
+#Valida que la presion sea numerica
+def validar_presion(presion):
+    try:
+        presion = float(presion)
+        return presion >= 0:
+    except (ValueError,TypeError):
+        return False
+    
+#Valida que la unidad sea PSI o bar 
+def validar_unidad(unidad):
+    
+    if unidad not in ["PSI", "BAR"]:
+        return False
+    return True
+
+
+#Valida cada una dato
+def validar_datos(id_revision,vehiculo,presion,unidad,tipo_vehiculo):
+    
+    if not id_revision or not str(id_revision).strip():
+        return False, "id vacio o invalido"
+    
+    if not vehiculo or not str(vehiculo).strip():
+        return False, "Vehiculo vacio o invalido"
+    
+    if not validar_presion(presion):
+        return False, "Presion invalida"
+    
+    if not validar_unidad(unidad):
+        return False, "Unidad invalida"
+    
+    if not tipo_vehiculo or not str(tipo_vehiculo).strip():
+        return False, "Tipo de vehiculo vacio o invalido"
+    
+    return True, None
+
+
+    
